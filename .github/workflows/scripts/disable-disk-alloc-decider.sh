c97af9c6a76c411bf70f0774a9b4f7517177f157
Amirhossein Baghaie <amir@tofisa.com>
2024-01-19 16:21:57 +0330

    Update disable-disk-alloc-decider.sh

--- .github/workflows/scripts/disable-disk-alloc-decider.sh
+++ .github/workflows/scripts/disable-disk-alloc-decider.sh
@@ -8,17 +8,17 @@ source "${BASH_SOURCE[0]%/*}"/lib/testing.sh
 
 
 cid_es="$(container_id elasticsearch)"
-ip_es="$(service_ip elasticsearch)"
+# ip_es="$(service_ip elasticsearch)"
 
 grouplog 'Wait for readiness of Elasticsearch'
-poll_ready "$cid_es" "http://${ip_es}:9200/" -u 'elastic:testpasswd'
+poll_ready "$cid_es" "http://127.0.0.1:9200/" -u 'elastic:testpasswd'
 endgroup
 
 log 'Disabling disk allocation decider'
 
 declare -a put_args=( '-X' 'PUT' '--fail-with-body' '-s' '-u' 'elastic:testpasswd'
 	'-H' 'Content-Type: application/json'
-	"http://${ip_es}:9200/_cluster/settings?pretty"
+	"http://127.0.0.1:9200/_cluster/settings?pretty"
 	'-d' '{"persistent":{"cluster.routing.allocation.disk.threshold_enabled":false}}'
 )
 declare response
